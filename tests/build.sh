9bd38c0ca181f604f02609c4008a41a12b61d6f6
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-06-21 14:37:11 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+sed -i "s~django.middleware.csrf.CsrfViewMiddleware~# django.middleware.csrf.CsrfViewMiddleware~g" ./label_studio/core/settings/basy.py
+
 docker buildx build . --output type=docker,name=elestio4test/label-studio:latest | docker load
 
